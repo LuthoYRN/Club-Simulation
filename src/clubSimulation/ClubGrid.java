3c7d1f324b32ecaacec4b16ea798ef7734c65614
lutho <luthoyrn@gmail.com>
2023-08-21 18:49:42 +0200

    added Barman movements

--- src/clubSimulation/ClubGrid.java
+++ src/clubSimulation/ClubGrid.java
@@ -9,11 +9,11 @@ public class ClubGrid {
 	private final int x;
 	private final int y;
 	public  final int bar_y;
-   
 	private GridBlock exit;
 	private GridBlock entrance; //hard coded entrance
 	private final static int minX =5;//minimum x dimension
 	private final static int minY =5;//minimum y dimension
+   private int barManID;
 	
 	public PeopleCounter counter;
 	
@@ -70,49 +70,23 @@ public class ClubGrid {
 		return true;
 	}
 	
-	public synchronized GridBlock enterClub(PeopleLocation myLocation) throws InterruptedException  {
+	public GridBlock enterClub(PeopleLocation myLocation) throws InterruptedException  {
 		counter.personArrived(); //add to counter of people waiting 
 		entrance.get(myLocation.getID());   
     
     //my code
-      while ((counter.getInside()+1 > counter.getMax())) {
-         synchronized (entrance){
-          entrance.wait();}
+    synchronized (entrance){
+      while ((counter.getInside() == counter.getMax())) { 
+          entrance.wait();
+     }
     }
       counter.personEntered(); //add to counter
 		myLocation.setLocation(entrance);
 		myLocation.setInRoom(true);
 		return entrance;
   }
-	
-	public synchronized GridBlock move(GridBlock currentBlock,int step_x, int step_y,PeopleLocation myLocation) throws InterruptedException {  //try to move in 
-		
-		int c_x= currentBlock.getX();
-		int c_y= currentBlock.getY();
-		
-		int new_x = c_x+step_x; //new block x coordinates
-		int new_y = c_y+step_y; // new block y  coordinates
-		
-		//restrict i an j to grid
-		if (!inPatronArea(new_x,new_y)) {
-			//Invalid move to outside  - ignore
-			return currentBlock;
-		}
-
-		if ((new_x==currentBlock.getX())&&(new_y==currentBlock.getY())) //not actually moving
-			return currentBlock;
-		 
-		GridBlock newBlock = Blocks[new_x][new_y];
-		
-		if (!newBlock.get(myLocation.getID())) return currentBlock; //stay where you are
-			
-		currentBlock.release(); //must release current block
-		myLocation.setLocation(newBlock);
-		return newBlock;
-	} 
-	
-
-	public synchronized void leaveClub(GridBlock currentBlock,PeopleLocation myLocation)   {
+  
+  public synchronized void leaveClub(GridBlock currentBlock,PeopleLocation myLocation)   {
 			currentBlock.release();
 			counter.personLeft(); //add to counter
 			myLocation.setInRoom(false);
@@ -120,6 +94,33 @@ public class ClubGrid {
 			entrance.notifyAll();
          }
 	}
+	
+	public synchronized GridBlock move(GridBlock currentBlock,int step_x, int step_y,PeopleLocation myLocation) throws InterruptedException {  //try to move in 
+		int c_x= currentBlock.getX();
+		int c_y= currentBlock.getY();
+		
+		int new_x = c_x+step_x; //new block x coordinates
+		int new_y = c_y+step_y; // new block y  coordinates
+      
+		//restrict i an j to grid
+      if (myLocation.getID()!=barManID) { //barMan exception to this rule
+   		if (!inPatronArea(new_x,new_y)) {
+   			//Invalid move to outside  - ignore
+   			return currentBlock;
+   		}
+      }
+
+		if ((new_x==currentBlock.getX())&&(new_y==currentBlock.getY())) {//not actually moving
+			return currentBlock;}
+		 
+		GridBlock newBlock = Blocks[new_x][new_y];
+		if (!newBlock.get(myLocation.getID())) {
+      return currentBlock;} //stay where you are
+			
+		currentBlock.release(); //must release current block
+		myLocation.setLocation(newBlock);
+   	return newBlock;
+	} 
 
 	public GridBlock getExit() {
 		return exit;
@@ -140,7 +141,10 @@ public class ClubGrid {
 	public int getBar_y() {
 		return bar_y;
 	}
-
+   
+   public void setBarManID(int ID){
+      this.barManID = ID;
+   }
 }
 
 
